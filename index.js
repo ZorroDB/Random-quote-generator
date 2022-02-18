a102984e44bf1b3f49cc9a052077c1f2ed6e87d5
diego <diegoblom01@gmail.com>
2022-02-18 23:19:02 +0100

    Quote generator

---
+++ index.js
@@ -0,0 +1,14 @@
+var quotes = [
+    "“Be yourself; everyone else is already taken.” ― Oscar Wilde",
+    "“Be who you are and say what you feel, because those who mind don't matter, and those who matter don't mind.” ― Bernard M. Baruch",
+    "“You've gotta dance like there's nobody watching, Love like you'll never be hurt, Sing like there's nobody listening, And live like it's heaven on earth.” ― William W. Purkey",
+    "“We cannot solve problems with the kind of thinking we employed when we came up with them.” — Albert Einstein",
+    "“Learn as if you will live forever, live like you will die tomorrow.” — Mahatma Gandhi",
+    "“When you give joy to other people, you get more joy in return. You should give a good thought to happiness that you can give out.”— Eleanor Roosevelt",
+    '"Success is not final; failure is not fatal: It is the courage to continue that counts." — Winston S. Churchill',
+    '"It is better to fail in originality than to succeed in imitation." — Herman Melville'
+]
+function randomQuote(){
+    var randomNumber = Math.floor(Math.random() * (quotes.length));
+    document.getElementById('random_quote').innerHTML = quotes[randomNumber];
+}
